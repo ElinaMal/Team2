91cf6908c9a57636ec15833c0f9225c8aa63862d
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2024-11-21 10:25:24 -0800

    Movement script and main character stuff

---
+++ 2D Template/Assets/Scripts/MovementMainCharacter.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode left = KeyCode.A, right = KeyCode.D, up = KeyCode.W, down = KeyCode.S;
+    public float speed = 6;
+    public float maxSpeed = 8;
+    bool facingLeft = false;
+
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(left)) //check for the player HOLDING DOWN the left button
+        {
+            if (_rb.velocity.x < -maxSpeed) //sets a limit for speed
+            {
+                _rb.velocity = new Vector2(-maxSpeed, _rb.velocity.y);
+            }
+
+            //get the GameObject's Rigidbody2D component and set its velocity to be to the left at the given speed
+            _rb.AddForce(new Vector2(-1,0) * speed);
+
+            if (facingLeft == false)
+            {
+                facingLeft = true;
+                transform.Rotate(0, 180, 0);
+            }
+        }
+
+        if (Input.GetKey(right)) //check for the player HOLDING DOWN the right button
+        {
+            if (_rb.velocity.x > maxSpeed) //sets a limit for speed
+            {
+                _rb.velocity = new Vector2(maxSpeed, _rb.velocity.y);
+            }
+
+            //get the GameObject's Rigidbody2D component and set its velocity to be to the right at the given speed
+            _rb.AddForce(new Vector2(1,0) * speed);
+
+            if (facingLeft == true)
+            {
+                facingLeft = false;
+                transform.Rotate(0, 180, 0);
+            }
+        }
+
+        if (Input.GetKey(up)) //check for the player HOLDING DOWN the up button
+        {
+            if (_rb.velocity.y > maxSpeed) //sets a limit for speed
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, maxSpeed);
+            }
+
+            //get the GameObject's Rigidbody2D component and set its velocity to be up at the given speed
+            _rb.AddForce(new Vector2(0,1) * speed);
+        }
+
+        if (Input.GetKey(down)) //check for the player HOLDING DOWN the down button
+        {
+            if (_rb.velocity.y < -maxSpeed) //sets a limit for speed
+            {
+                _rb.velocity = new Vector2(_rb.velocity.x, -maxSpeed);
+            }
+
+            //get the GameObject's Rigidbody2D component and set its velocity to be down at the given speed
+            _rb.AddForce(new Vector2(0,-1) * speed);
+        }
+    }
+}
