631038572f29e892dc8ba78f336f28870cdf8615
ElinaMalice <1721537@mukilteo.wednet.edu>
2024-11-30 19:40:55 -0800

    Made it so that enemies stop for a bit every time they change direction

--- 2D Template/Assets/Scripts/EnemyMovement.cs
+++ 2D Template/Assets/Scripts/EnemyMovement.cs
@@ -12,13 +12,14 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float enemyVelocity;
     [SerializeField] private Vector2 movementDirection;
     [SerializeField] private Vector2 movement;
+    private bool wait = false;
 
     private Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        directionChangeTime = UnityEngine.Random.Range(1.0f, 5.0f);
+        directionChangeTime = UnityEngine.Random.Range(5.0f, 10.0f);
         lastDirectionChange = 0f;
         FindMovement();
     }
@@ -28,17 +29,30 @@ public class EnemyMovement : MonoBehaviour
     {
         if (Time.time - lastDirectionChange > directionChangeTime)
         {
+            StartCoroutine(StandingStill());
             lastDirectionChange = Time.time;
-            directionChangeTime = UnityEngine.Random.Range(1.0f, 5.0f);
+            directionChangeTime = UnityEngine.Random.Range(5.0f, 10.0f);
             FindMovement();
         }
 
-        _rb.velocity= new Vector2(movement.x, movement.y);
+        if (wait == false)
+        {
+            _rb.velocity = new Vector2(movement.x, movement.y);
+        }
     }
 
     void FindMovement()
     {
         movementDirection = new Vector2(UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f,1.0f));
         movement = movementDirection * enemyVelocity;
+    }
+
+    IEnumerator StandingStill()
+    {
+        Debug.Log("It started");
+        wait = true;
+        _rb.velocity = new Vector2(0, 0);
+        yield return new WaitForSeconds(3f);
+        wait = false;
     }
 }
