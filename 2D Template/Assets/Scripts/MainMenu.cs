d5c6f5195637f128ef5eae651a28f694ab2a851d
MUKILTEO\1714309 <mervosepenu96@gmail.com>
2024-12-05 10:19:58 -0800

    =UI Main Menu

--- 2D Template/Assets/Scripts/MainMenu.cs
+++ 2D Template/Assets/Scripts/MainMenu.cs
@@ -12,8 +12,8 @@ public class MainMenu : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
