45c002ef4a8934a461ef45c20e5a5e50fa0ae0c9
MUKILTEO\1721634 <vorochi7606@gmail.com>
2024-12-12 10:02:28 -0800

    Bullet script finally not bugged

--- 2D Template/Assets/Scripts/BulletVisual.cs
+++ 2D Template/Assets/Scripts/BulletVisual.cs
@@ -6,4 +6,30 @@ public class BulletVisual : MonoBehaviour
 {
     [SerializeField] private Transform projectileVisual;
     [SerializeField] private Transform projectileShadow;
+    [SerializeField] private BulletScript bulletScript;
+
+    //private Vector3 target;
+    //private Vector3 trajectoryStartPosition;
+
+    private void Update()
+    {
+        UpdateProjectileRotation();
+    }
+
+    //private void UpdateShadowPosition()
+    //{
+    //}
+
+    private void UpdateProjectileRotation()
+    {
+        Vector3 projectileMoveDir = bulletScript.GetProjectileMoveDir();
+
+        projectileVisual.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(projectileMoveDir.y, projectileMoveDir.x) * Mathf.Rad2Deg);
+        projectileShadow.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(projectileMoveDir.y, projectileMoveDir.x) * Mathf.Rad2Deg);
+    }
+
+    //public void SetTarget(Vector3 target)
+    //{
+    //    this.target = target;
+    //}
 }
