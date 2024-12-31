2e3834a871938caa6bff1aa805ee10a88e2fe29b
ElinaMalice <1721537@mukilteo.wednet.edu>
2024-12-31 15:46:32 -0800

    Timer has been added

--- 2D Template/Assets/Scripts/EnemyScripts/EnemyNumberTracker.cs
+++ 2D Template/Assets/Scripts/EnemyScripts/EnemyNumberTracker.cs
@@ -1,7 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Mathematics.math;
 
 public class EnemyNumberTracker : MonoBehaviour
 {
     public int enemyCounter = 0;
     public int undeadCounter = 0;
+    public float timePassed = 0;
+    public float trueTime;
+    public TMPro.TMP_Text scoreText;
+
+    void Update() 
+    {
+        timePassed = Time.time;
+        trueTime = (Mathf.Round(timePassed * 100)) / 100;
+        scoreText.SetText(trueTime.ToString());
+    }
 }
